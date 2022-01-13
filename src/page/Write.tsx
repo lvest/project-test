fed08d6750a5accf84312a665690a7904906566c
lvest <ookkid.1@gmail.com>
2022-01-13 09:58:49 +0900

    빌드 전 커밋

--- src/page/Write.tsx
+++ src/page/Write.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Write() {
-  return <div></div>;
+  const Form = styled.form``;
+  return (
+    <Form>
+      <div>
+        <label>Enter your name: </label>
+        <input type='text' name='name' id='name' required />{' '}
+      </div>
+      <div>
+        <input type='email' name='email' id='email' required />
+      </div>
+      <div>
+        <input type='submit' value='Subscribe!' />
+      </div>
+    </Form>
+  );
 }
 
 export default Write;
