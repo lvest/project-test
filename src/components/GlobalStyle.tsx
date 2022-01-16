1c485bd705cf0dbb179837ac36ae5648b0e8548d
lvest <ookkid.1@gmail.com>
2022-01-16 12:42:13 +0900

    글쓰기 페이지, 이미지 등록

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -11,6 +11,15 @@ const GlobalStyle = createGlobalStyle`
     margin: 8vh 0;
     background-color: aliceblue;
   }
+
+  input{
+    width: 80vw;
+    height: 7vh;
+    padding: 1vh;
+    background-color: transparent;
+    border: 1px solid black;
+    border-radius: 10px;
+  }
 `;
 
 export default GlobalStyle;
--- src/page/Write.tsx
+++ src/page/Write.tsx
@@ -1,23 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Form = styled.form`
+  width: 100vw;
+  height: 84vh;
+  padding: 1vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1vw;
+  background-color: palegoldenrod;
+`;
+
+const Photo = styled.input``;
+
+const Title = styled.input``;
+
+const Price = styled.input``;
+
+const ContentWrapper = styled.div``;
+
+const Content = styled.input`
+  height: 50vh;
+`;
+
+const Submit = styled.input`
+  background-color: wheat;
+`;
+
 function Write() {
-  const Form = styled.form`
-    width: 100vw;
-  `;
   return (
-    <Form>
-      <div>
-        <label>Enter your name: </label>
-        <input type='text' name='name' id='name' required />{' '}
-      </div>
-      <div>
-        <input type='email' name='email' id='email' required />
-      </div>
-      <div>
-        <input type='submit' value='Subscribe!' />
-      </div>
-    </Form>
+    <main>
+      <Form>
+        <div>
+          <Photo type='file' placeholder='사진을 등록하세요' />
+        </div>
+        <div>
+          <Title type='text' placeholder='제목을 입력하세요' required />{' '}
+        </div>
+        <div>
+          <Price type='text' placeholder='가격을 입력하세요' required />
+        </div>
+        <div>
+          <Content type='text' placeholder='내을 입력하세요' required />
+        </div>
+        <Submit type='submit' value='등록' />
+      </Form>
+    </main>
   );
 }
 
